6c692e85fd0804805be876636df69b7618d81ae7
Huevos <rowland@moralet.com>
2024-06-27 00:42:37 +0200

    Use sanitizeFilename like in Picon.py

--- src/picons-missing.py
+++ src/picons-missing.py
@@ -58,7 +58,7 @@ def missingPicons():
 			paths[n] = f
 	del(pathsSplit)
 	print("Reading lamedb...")
-	f = open(lamedb).readlines()
+	f = open(lamedb, mode="r", encoding="UTF8", errors="ignore").readlines()
 	f = f[f.index("services\n")+1:-3]
 	i = 0
 	done = []
@@ -78,7 +78,8 @@ def missingPicons():
 		#ref = f[0][:-1] # service ref string from lamedb
 		oldPiconName = refstr.replace(':','_') + ".png"
 		oldPiconName2 = refstr2.replace(':','_') + ".png"
-		newName1 = unicodedata.normalize('NFKD', str(name)).encode('ASCII', 'ignore').decode('ASCII', 'ignore')
+		# newName1 = unicodedata.normalize('NFKD', str(name)).encode('ASCII', 'ignore').decode('ASCII', 'ignore')
+		newName1 = sanitizeFilename(name)
 		newName2 = re.sub("[^a-z0-9]", "", newName1.replace("&", "and").replace("+", "plus").replace("*", "star").lower())
 		newPiconName = newName2  + ".png"
 		if int(ref[4]) < 26: # service type in decimal
@@ -276,5 +277,51 @@ def satname(slot):
 	
 def slot1(namespace):
 	return int(namespace[:len(namespace)-4], 16)
+
+def sanitizeFilename(filename):  # from Directories.py
+	"""Return a fairly safe version of the filename.
+
+	We don't limit ourselves to ascii, because we want to keep municipality
+	names, etc, but we do want to get rid of anything potentially harmful,
+	and make sure we do not exceed Windows filename length limits.
+	Hence a less safe blacklist, rather than a whitelist.
+	"""
+	blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0"]
+	reserved = [
+		"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
+		"COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+		"LPT6", "LPT7", "LPT8", "LPT9",
+	]  # Reserved words on Windows
+	filename = "".join(c for c in filename if c not in blacklist)
+	# Remove all charcters below code point 32
+	filename = "".join(c for c in filename if 31 < ord(c))
+	filename = unicodedata.normalize("NFKD", filename)
+	filename = filename.rstrip(". ")  # Windows does not allow these at end
+	filename = filename.strip()
+	if all([x == "." for x in filename]):
+		filename = "__" + filename
+	if filename in reserved:
+		filename = "__" + filename
+	if len(filename) == 0:
+		filename = "__"
+	if len(filename) > 255:
+		parts = split(r"/|\\", filename)[-1].split(".")
+		if len(parts) > 1:
+			ext = "." + parts.pop()
+			filename = filename[:-len(ext)]
+		else:
+			ext = ""
+		if filename == "":
+			filename = "__"
+		if len(ext) > 254:
+			ext = ext[254:]
+		maxl = 255 - len(ext)
+		filename = filename[:maxl]
+		filename = filename + ext
+		# Re-check last character (if there was no extension)
+		filename = filename.rstrip(". ")
+		if len(filename) == 0:
+			filename = "__"
+	return filename
 			
 missingPicons()
